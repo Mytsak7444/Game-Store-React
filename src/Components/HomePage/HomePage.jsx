a8687596a53b9db3f6a89324a7c6da99255615c9
Mytsak Vasyl <mega.mitsak@ukr.net>
2023-03-07 13:54:41 +0200

    Change all photo

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -15,7 +15,8 @@ const GAMES = [
       "На Вас чекає нескінченний калейдоскоп пригод Horizon! Здійснюйте захоплюючі поїздки неймовірно красивим і самобутнім світом Мексики за кермом найбільших автомобілів в історії. Почніть свою пригоду Horizon вже сьогодні, додавши гру до свого списку бажань!",
   },
   {
-    image: "/public/game-img/battlefield_2042.jpg",
+    image:
+      "https://media.contentapi.ea.com/content/dam/battlefield/battlefield-2042/images/2021/04/k-1920x1080-featured-image.jpg.adapt.crop191x100.1200w.jpg",
     title: "Battlefield 2042",
     genres: ["Екшн", "Шутер", "Війна"],
     video: "https://www.youtube.com/embed/ASzOzrB-a9E",
@@ -25,7 +26,8 @@ const GAMES = [
       "Battlefield™ 2042 - це шутер від першої особи, в якому серія повертається до легендарних масштабних битв. У майбутньому, де панує хаос, адаптуйтеся і процвітайте на полях бою, що постійно змінюються, завдяки своєму загону і арсеналу новітніх технологій.",
   },
   {
-    image: "/public/game-img/life_is_strange_true_colors.jpeg",
+    image:
+      "https://n8t8d9t2.rocketcdn.me/wp-content/uploads/2021/09/gm9295.jpg",
     title: "Life is Strange True Colors",
     genres: ["Глибокий сюжет", "Протагоністка"],
     video: "https://www.youtube.com/embed/b6CkzwVAr0M",
@@ -35,7 +37,8 @@ const GAMES = [
       "Алекс Чень від усіх приховує своє «прокляття» — надприродну здатність зчитувати сильні емоції інших та впливати на них. Але коли її брат гине — нібито внаслідок нещасного випадку, Алекс використовує її, щоб дізнатися правду.",
   },
   {
-    image: "/public/game-img/gta_v.jpeg",
+    image:
+      "https://variety.com/wp-content/uploads/2013/09/gta-v-big.jpg?w=681&h=383&crop=1",
     title: "Grand Theft Auto V",
     genres: ["Відкритий світ", "Екшн"],
     video: "https://www.youtube.com/embed/QkkoHAzjnUs",
@@ -45,7 +48,8 @@ const GAMES = [
       "Grand Theft Auto V для PC дозволяє гравцям дослідити знаменитий світ Лос-Сантоса та округу Блейн у роздільній здатності до 4k і вище з частотою 60 кадрів на секунду.",
   },
   {
-    image: "/public/game-img/rainbow_siege.jpeg",
+    image:
+      "https://s2.gaming-cdn.com/images/products/406/616x353/tom-clancy-s-rainbow-six-siege-pc-game-ubisoft-connect-europe-cover.jpg?v=1649421231",
     title: "Tom Clancy's Rainbow Six® Siege",
     video: "https://www.youtube.com/embed/6wlvYh0h63k",
     genres: ["Тактика", "Шутер"],
@@ -55,7 +59,8 @@ const GAMES = [
       "Tom Clancy's Rainbow Six Облога - це продовження гучного шутера від першої особи, розробленої студією Ubisoft Montreal.",
   },
   {
-    image: "/public/game-img/assassins_creed_valhalla.png",
+    image:
+      "https://cdn.akamai.steamstatic.com/steam/apps/2208920/capsule_616x353.jpg?t=1671135934",
     title: "Assassin’s Creed Valhalla",
     genres: ["Паркур", "РПГ", "Відкритий світ"],
     video: "https://www.youtube.com/embed/ssrNcwxALS4",
